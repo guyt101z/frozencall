44031ec260daf400c93e2c789c50c209eb7fa91c
Adolfo Maltez <adolfomaltez@gmail.com>
2012-02-27 11:54:16 -0600

    Agent main screen, and added some icons

--- agi/linkCall.php
+++ agi/linkCall.php
@@ -1,18 +1,28 @@
 <?php
-require_once ('../inc/phpagi-asmanager.php');
-$server = "127.0.0.1";
-$username = "foo"; // asterisk manager username
-$secret = "bar";
+/*
+* linkCall
+* link call betwen agent and lead
+*/
+
+
+set_time_limit(30);
+error_reporting(E_ALL);
+
+
+
+include('../inc/config.php');
+require_once('../inc/phpagi-asmanager.php');
+
 $trunk = "SIP/myvoipprovider";
 
 
 // here we have some logic to retrieve the data..
-$agent_number = "1001";
-$client_number = "1002";
+$agent_number = "1001";  //get from system session
+$client_number = "1002"; // get from leads table in the database
 
 $manager = new AGI_AsteriskManager();
-$con = $manager->connect($server,$username,$secret);
+$con = $manager->connect($AMI_IP, $AMI_USER, $AMI_PASS);
 
 //$orig = $manager->Originate("$trunk/$client_number", NULL, NULL, NULL, "Dial", "SIP/$agent_number");
-$orig = $manager->Originate("SIP/1002", NULL, NULL, NULL, "Dial", "SIP/1001");
+$orig = $manager->Originate("SIP/1002", NULL, NULL, NULL, "Dial", "SIP/1001", NULL, "Call Center");
 ?>
--- gui/agent/index.php
+++ gui/agent/index.php
@@ -4,8 +4,97 @@
 */
 
 ?>
+<html>
+<script type="text/javascript" src="../jquery-1.7.1.min.js"></script>
 
-<form action="#" method="post">
-<input type="submit" value="Dial next">
-<input type="submit" value="MUTE">
-</form>
+<script>
+
+function linkCall(){
+    $.ajax({
+        dataType: "json",
+        url: "../agi/linkCall.php",
+        success: function (data){
+        }
+    });
+}
+</script>
+
+
+
+<table width="1024" border="0">
+<tr>
+<td>
+<div style="height:80px;border:1px solid blue;top:0">
+[Logo Empresa]
+</div>
+</td>
+<td>
+<div style="height:80px;border:1px solid red;top:0">
+Fecha: Hora:Segundos Dia/Mes/Anio
+</div>
+</td>
+<td>
+<div style="height:80px;border:1px solid blue;top:0">
+Usuario: Foo / 1234<br>
+Campania: Bar
+<img style="align:rigth" src="../icons/logout.png"><br>
+</div>
+</td>
+</tr>
+
+
+
+<tr>
+
+
+<td width="200" style="vertical-align:top">
+<div style="height:80px;border:1px solid blue;top:0">
+<img src="../icons/phone.png" onclick="linkCall();">
+<br>
+Automatic DIAL.
+<br>
+<br>
+</div>
+<div style="height:320px;border:1px solid red;margin-top:0">
+[Teclado Telefono]
+</div>
+</td>
+
+
+
+<td height="400">
+<div style="height:400px;border:1px solid red;top:0">
+<div>
+Encuesta
+
+</td>
+
+
+<td width="200" style="vertical-align:top">
+<div style="height:80px;border:1px solid blue;top:0">
+<img src="../icons/call-start.png" onclick="foo();">
+<br>
+Call in Progress.
+</div>
+<div style="height:220px;border:1px solid red;margin-top:0">
+[X]
+</div>
+<div style="height:100px;border:1px solid green;margin-top:0">
+<img src="../icons/muted.png" onclick="Mute();">
+<img src="../icons/up.png" onclick="up();">
+<img src="../icons/down.png" onclick="down();">
+<br>
+Sound Panel.
+</div>
+</td>
+</tr>
+<tr>
+</tr>
+<tr>
+<th width="30" colspan="3"  style="vertical-align:top">
+<div style="height:30px;border:1px solid blue;top:0">
+</div>
+</th>
+</tr>
+</table>
+</html>
